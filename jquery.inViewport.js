7014a9127cb2d3259b61f62d0eb9c954cc5d6547
Kaanon MacFarlane <kaanonm@gmail.com>
2014-03-09 10:51:30 -0700

    Create jquery.inViewport.js

---
+++ jquery.inViewport.js
@@ -0,0 +1,21 @@
+(function ($) {
+    var $window = $(window),
+        windowHeight = $window.height();
+    $.fn.inViewport = function () {
+        var scrollPos = $window.scrollTop(),
+            windowBottom = scrollPos + windowHeight,
+            bottom = scrollPos + windowHeight,
+            sectionHeight = this.height(),
+            sectionTop = this.offset().top,
+            sectionBottom = sectionTop + sectionHeight,
+            visible = (bottom > sectionTop) && (scrollPos < sectionBottom);
+        return visible;
+    };
+    $.fn.pixelsIn = function () {
+        var scrollPos = $window.scrollTop(),
+            windowBottom = scrollPos + windowHeight,
+            sectionTop = this.offset().top,
+            pixelsIn = windowBottom - sectionTop;
+        return pixelsIn;
+    };
+}(jQuery));
